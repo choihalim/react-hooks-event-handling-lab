2e9bbfc47b81b07d0827ee4ed3bf51af076f743b
Halim Choi <hchoicareer@gmail.com>
2023-04-11 16:14:45 -0400

    completed lab

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe() {
+
+    function reportFocus() {
+        console.log("Good!");
+    }
+
+    function reportBlur() {
+        console.log("Hey! Eyes on me!");
+    }
+
+    return (
+        <button onFocus={reportFocus} onBlur={reportBlur}>Eyes on me</button>
+    );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
-function Keypad (){
+function Keypad() {
+
+    function reportChange() {
+        console.log('Entering password...');
+    }
+
     return (
-        <div></div>
+        <>
+            <input type="password" onChange={reportChange}></input>
+        </>
     )
 }
 
